ab7c2070eccc424f648bae3fa5ddcb39a4b61d9e
Brenno-Miranda <brennomiranda12345@gmail.com>
2026-07-14 17:57:28 +0000

    Adiciona página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Eai minha gata, estou fazendo o curso e um dos desafios é mandar uma mensagem pra alguém através da página. TE AMO!!</h1>
+}
+
+export default Home;
