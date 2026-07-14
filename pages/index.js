d299bb88316e00deee3e8a8456e74939f0ef8b6c
Brenno-Miranda <brennomiranda12345@gmail.com>
2026-07-14 18:56:21 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eai minha gata, estou fazendo o curso e um dos desafios é mandar uma mensagem pra alguém através da página. TE AMO!!</h1>
+    return <h1>Eai minha gata, estou fazendo o curso e um dos desafios é mandar uma mensagem pra alguém através da página. TE AMO MINHA GATA!!</h1>
 }
 
 export default Home;
